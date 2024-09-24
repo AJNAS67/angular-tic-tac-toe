aa70a56d21316aa806bc18a0ca35c7ddaaab0ac7
Ajnas kp <ajnaskp67@gmail.com>
2024-09-23 19:19:58 -0700

    create squere component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NbLayoutModule, NbButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-tic-tac-toe';
