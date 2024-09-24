5d6b29ba566ed0c87025e8f026d44da2a4976efb
Ajnas kp <ajnaskp67@gmail.com>
2024-09-23 19:20:15 -0700

    create board component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbButtonModule,
+  NbSidebarModule,
+} from '@nebular/theme';
+import { BoardComponent } from './components/board/board.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NbLayoutModule, NbButtonModule],
+   
+    imports: [RouterOutlet,BoardComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/square.ts
+++ src/app/components/square.ts
@@ -2,15 +2,26 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
+  standalone: true,
   template: `
-    <button nbButton *ngIf="!value">{{ value }}</button>
-    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
-    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+    <!-- <button nbButton *ngIf="!value">{{ value }}</button> -->
+    @if (!value) {
+        <button>{{value}}</button>
+    }
+    @if(value=='X'){
+
+        <button style="background-color: aqua;" status="success">{{ value }}</button>
+    }
+    @if(value=='O'){
+    <button  style="background-color: bisque;" status="info">{{ value }}</button>
+
+    }
   `,
   styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent  {
 
-  @Input() value: 'X' | 'O' | undefined;
+  @Input()
+    value!: 'X' | 'O';
 
 }
